242abe6e1985d909e67e827033bd03c1e9521f77
Alberto Fernandez <linuxxroot@gmail.com>
2024-07-28 00:14:19 -0600

    Create new terraform file

---
+++ aws-env/create-ubuntu-server.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region = "us-east-1" # Specify your region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a" # Specify an availability zone in the us-east-1 region
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_security_group" "jenkins" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins_server" {
+  ami                    = "ami-04b70fa74e45c3917"
+  instance_type          = "t2.medium"
+  key_name               = "aws-access-key"
+  associate_public_ip_address = true
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.jenkins.id]
+
+  root_block_device {
+    volume_size = 30
+  }
+
+  tags = {
+    Name = "JenkinsServer"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update -y
+    sudo apt upgrade -y
+  EOF
+
+  depends_on = [aws_security_group.jenkins]
+}
+
+resource "aws_s3_bucket" "jenkins_artifacts" {
+  bucket = "jenkins-artifacts-${random_string.bucket_suffix.result}"
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
